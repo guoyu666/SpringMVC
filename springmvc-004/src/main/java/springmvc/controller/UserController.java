7349de52f571e03af85cb9b23471473d221cb6e1
GuoYu <1262425287@qq.com>
2024-05-23 23:30:42 +0800

    提交代码修改

--- springmvc-004/src/main/java/springmvc/controller/UserController.java
+++ springmvc-004/src/main/java/springmvc/controller/UserController.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import springmvc.pojo.User;
 
-import java.util.Arrays;
-
 @Controller
 public class UserController {
 
